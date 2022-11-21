44c298d39323d527da71ef4abbfbf940ec90c2b9
ibrokhim <mr017programmer@gmail.com>
2022-11-21 21:28:34 +0500

    mongoDB not working, tekshirish kerak

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 import express from "express";
-// import jwt from "jsonwebtoken";
-// import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
+// custom files
+import UserModel from "./models/User.js";
 import { registerValidation } from "./validations/auth.js";
+
+mongoose
+	.connect("mongodb+srv://admin:adminqwerty@cluster0.glxeezl.mongodb.net/?retryWrites=true&w=majority")
+	.then(() => console.log("DB run"))
+	.catch((err) => console.log("DB err =>", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
 		return res.status(400).json(errors.array());
 	}
+
+	const password = req.body.password;
+	const salt = await bcrypt.genSalt(10);
+	const passwordHash = await bcrypt.hash(password, salt);
+
+	const doc = UserModel({
+		email: req.body.email,
+		fullName: req.body.fullName,
+		avatarUrl: req.body.avatarUrl,
+		passwordHash: passwordHash,
+	});
+
+	const user = await doc.save();
 
 	res.json({
 		req: req.body,
@@ -28,10 +49,3 @@ app.listen(4444, (err) => {
 	}
 	console.log("Server runnig");
 });
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://admin:adminqwerty@cluster0.glxeezl.mongodb.net/?retryWrites=true&w=majority"
-//   )
-//   .then(() => console.log("DB run"))
-//   .catch((err) => console.log("DB err =>", err));
