a91e0a4e4f6a36dfa13714eea57672283d176f79
ibrokhim <mr017programmer@gmail.com>
2022-11-21 21:10:04 +0500

    fix

--- index.js
+++ index.js
@@ -5,22 +5,12 @@ import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://admin:adminqwerty@cluster0.glxeezl.mongodb.net/?retryWrites=true&w=majority"
-//   )
-//   .then(() => console.log("DB run"))
-//   .catch((err) => console.log("DB err =>", err));
-
 const app = express();
 
 app.use(express.json());
 
 app.post("/auth/register", registerValidation, (req, res) => {
-	console.log("req >>> ", req.body);
-	// ,
 	const errors = validationResult(req);
-	console.log("errors", errors);
 
 	if (!errors.isEmpty()) {
 		return res.status(400).json(errors.array());
@@ -28,7 +18,7 @@ app.post("/auth/register", registerValidation, (req, res) => {
 
 	res.json({
 		req: req.body,
-		success: true
+		success: true,
 	});
 });
 
@@ -38,3 +28,10 @@ app.listen(4444, (err) => {
 	}
 	console.log("Server runnig");
 });
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://admin:adminqwerty@cluster0.glxeezl.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => console.log("DB run"))
+//   .catch((err) => console.log("DB err =>", err));
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-	body("email", "Email xato kiritilgan").isEmail(),
-	body("password", "Parol 5ta simvoldan ko'p bo'lishi kerak").isLength({ min: 5 }),
-	body("fullName", "fullName 3ta simvoldan ko'p bo'lishi kerak").isLength({ min: 3 }),
-	body("avatarUrl", "Rasm url to'g'ri kiritilmadi").optional().isURL()
+	body("email", "Pochta xato kitirilgan").isEmail(),
+	body("password", "Parol 5ta simvoldan kam bo'lmasin").isLength({ min: 5 }),
+	body("fullName", "Isim 3ta simvoldan kam bo'lmasin").isLength({ min: 3 }),
+	body("avatarUrl", "Fayl url xato kiritilgan").optional().isURL(),
 ];
